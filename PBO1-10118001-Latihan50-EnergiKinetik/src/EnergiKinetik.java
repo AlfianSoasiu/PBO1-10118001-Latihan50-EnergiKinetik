9b451dc1701a90c6095dbfc4e93ec241315cfc2f
Alfian Soasiu <55717248+AlfianSoasiu@users.noreply.github.com>
2019-11-08 00:01:15 +0700

    Add files via upload

---
+++ PBO1-10118001-Latihan50-EnergiKinetik/src/EnergiKinetik.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.text.DecimalFormat;
+
+/**
+ *
+ * @author 
+ * Nama      : Alfian Soasiu
+ * Kelas     : PBO1
+ * NIM       : 10118001
+ * Deskripsi : Menampilkan output energi kinetik
+ */
+public class EnergiKinetik {
+
+    public static void main(String[] args) {
+        
+        Energi kinetik = new Energi();
+        kinetik.setMassa(0.145);
+        kinetik.setKecepatan(25);
+        kinetik.setKecepatanAwal(0);
+        kinetik.setKecepatanAkhir(25);
+        
+        DecimalFormat df = new DecimalFormat("#.#");
+        
+        System.out.println("______PROGRAM MENCARI ENERGI KINETIK______");
+        System.out.println("Energi Kinetik\t : " + df.format(
+                           kinetik.hitungEnergiKinetik
+                          (kinetik.getMassa(), kinetik.getKecepatan())));
+        System.out.println("Usaha\t\t : " + df.format (kinetik.hitungUsaha
+                          (kinetik.getKecepatanAwal(), 
+                           kinetik.getKecepatanAkhir())));
+    }
+    
+}
